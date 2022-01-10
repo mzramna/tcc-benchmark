96657aa8b8f95c6bce2fc8433c4dbac812affbfa
miguel Lopes <mzramnadev@gmail.com>
2022-01-10 17:12:54 -0300

    comunicação paralela com erro,perde conexão por algum motivo

--- scripts/teste_script_bd_completo_paralel.py
+++ scripts/teste_script_bd_completo_paralel.py
@@ -94,7 +94,7 @@ logstash_data={}
 gerenciador=GerenciadorDeBD(host="192.168.0.100", user="mzramna", password="safePassword", database="sakila", port=3306,tipo=0,sql_file_pattern="containers_build/mysql default exemple.sql",logstash_data=logstash_data)
 #reset
 gerenciador.reset_database()
-#gerenciador.execute_operation_from_sqlite_no_return(2000, "scripts/initial_db.db")
+
 for i in range(len(threads)):
     threads[i]=Worker(q)
     threads[i].exec_function(function= gerenciador.execute_operation_from_sqlite_no_return_with_id,index_retorno=i)
