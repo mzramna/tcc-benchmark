32640c05263e71ef6580538214bb8bd1512fc8fd
miguel Lopes <mzramnadev@gmail.com>
2022-01-18 18:48:05 -0300

    melhorias nos scripts de inicialização

--- containers_build/alpine/install_alpine_final.sh
+++ containers_build/alpine/install_alpine_final.sh
@@ -2,14 +2,19 @@
 read -sp 'senha root: ' senha
 # read -sp 'senha root opi-pc01: ' senha
 echo ""
+build=""#"--build"
+force="--force-recreate"
+start="-d "#"--no-start"
 sshpass -p "$senha"  rsync -airup --progress ~/tcc-benchmark/ mzramna@192.168.0.10:~/tcc-benchmark/ --exclude=".env" --exclude="dados.json" --exclude="meuvenv"
 sshpass -p  "$senha"  ssh root@192.168.0.10 "cd /home/mzramna/tcc-benchmark/containers_build/alpine && sudo docker-compose stop"
-sshpass -p  "$senha"  ssh root@192.168.0.10  "sudo rm -rf /media/docker_mount/testes_tcc/*/*/* "
-sshpass -p  "$senha"  ssh root@192.168.0.10 "cd /home/mzramna/tcc-benchmark/containers_build/alpine && sudo docker-compose up --build --force-recreate -d --no-start"
+sshpass -p  "$senha"  ssh root@192.168.0.10  "sudo rm -rf /media/docker_mount/testes_tcc/*/data/* /media/docker_mount/testes_tcc/*/data/*"
+#sshpass -p  "$senha"  ssh root@192.168.0.100  " rsync -airup /home/mzramna/tcc-benchmark/containers_build/alpine/alpine_mariadb/config/dados.json /media/docker_mount/testes_tcc/mariadb/config/ &&  rsync -airup /home/mzramna/tcc-benchmark/containers_build/alpine/alpine_postgres/config/dados.json /media/docker_mount/testes_tcc/postgres/config/ && chown mzramna -R /media/docker_mount/testes_tcc/"
+sshpass -p  "$senha"  ssh root@192.168.0.10 "cd /home/mzramna/tcc-benchmark/containers_build/alpine && sudo docker-compose up $start $build $force"
 
 #read -sp 'senha root server-testes: ' senha
 #echo ""
 sshpass -p "$senha"  rsync -airup --progress ~/tcc-benchmark/ mzramna@192.168.0.20:~/tcc-benchmark/ --exclude=".env" --exclude="dados.json" --exclude="meuvenv"
 sshpass -p  "$senha"  ssh root@192.168.0.20 "cd /home/mzramna/tcc-benchmark/containers_build/alpine && sudo  docker-compose stop"
-sshpass -p  "$senha"  ssh root@192.168.0.20  "sudo rm -rf /media/docker_mount/testes_tcc/*/*/* "
-sshpass -p  "$senha"  ssh root@192.168.0.20 "cd /home/mzramna/tcc-benchmark/containers_build/alpine && sudo docker-compose up --build --force-recreate -d --no-start"
+sshpass -p  "$senha"  ssh root@192.168.0.20  "sudo rm -rf /media/docker_mount/testes_tcc/*/data/* /media/docker_mount/testes_tcc/*/data/*"
+#sshpass -p  "$senha"  ssh root@192.168.0.100  " rsync -airup /home/mzramna/tcc-benchmark/containers_build/alpine/alpine_mariadb/config/dados.json /media/docker_mount/testes_tcc/mariadb/config/ &&  rsync -airup /home/mzramna/tcc-benchmark/containers_build/alpine/alpine_postgres/config/dados.json /media/docker_mount/testes_tcc/postgres/config/ && chown mzramna -R /media/docker_mount/testes_tcc/"
+sshpass -p  "$senha"  ssh root@192.168.0.20 "cd /home/mzramna/tcc-benchmark/containers_build/alpine && sudo docker-compose up $start $build $force "
--- containers_build/alpine_no_daemon/install_alpine_final.sh
+++ containers_build/alpine_no_daemon/install_alpine_final.sh
@@ -2,14 +2,16 @@
 read -sp 'senha root: ' senha
 # read -sp 'senha root opi-pc01: ' senha
 echo ""
+build="--build"
+force="--force-recreate"
 sshpass -p "$senha"  rsync -airup --progress ~/tcc-benchmark/ mzramna@192.168.0.10:~/tcc-benchmark/ --exclude=".env" --exclude="dados.json" --exclude="meuvenv"
 sshpass -p  "$senha"  ssh root@192.168.0.10 "cd /home/mzramna/tcc-benchmark/containers_build/alpine_no_daemon && sudo docker-compose stop"
-sshpass -p  "$senha"  ssh root@192.168.0.10  "sudo rm -rf /media/docker_mount/testes_tcc/*/*/* "
-sshpass -p  "$senha"  ssh root@192.168.0.10 "cd /home/mzramna/tcc-benchmark/containers_build/alpine_no_daemon && sudo docker-compose up --build --force-recreate -d --no-start"
+sshpass -p  "$senha"  ssh root@192.168.0.10  "sudo rm -rf /media/docker_mount/testes_tcc/*/data/* /media/docker_mount/testes_tcc/*/data/*"
+sshpass -p  "$senha"  ssh root@192.168.0.10 "cd /home/mzramna/tcc-benchmark/containers_build/alpine_no_daemon && sudo docker-compose up $build $force -d --no-start"
 
 #read -sp 'senha root server-testes: ' senha
 #echo ""
 sshpass -p "$senha"  rsync -airup --progress ~/tcc-benchmark/ mzramna@192.168.0.20:~/tcc-benchmark/ --exclude=".env" --exclude="dados.json" --exclude="meuvenv"
 sshpass -p  "$senha"  ssh root@192.168.0.20 "cd /home/mzramna/tcc-benchmark/containers_build/alpine_no_daemon && sudo  docker-compose stop"
-sshpass -p  "$senha"  ssh root@192.168.0.20  "sudo rm -rf /media/docker_mount/testes_tcc/*/*/* "
-sshpass -p  "$senha"  ssh root@192.168.0.20 "cd /home/mzramna/tcc-benchmark/containers_build/alpine_no_daemon && sudo docker-compose up --build --force-recreate -d --no-start"
+sshpass -p  "$senha"  ssh root@192.168.0.20  "sudo rm -rf /media/docker_mount/testes_tcc/*/data/* /media/docker_mount/testes_tcc/*/data/*"
+sshpass -p  "$senha"  ssh root@192.168.0.20 "cd /home/mzramna/tcc-benchmark/containers_build/alpine_no_daemon && sudo docker-compose up $build $force -d --no-start"
