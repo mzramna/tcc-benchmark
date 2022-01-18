477ca48ccff7ed2bc209bd6ef249b11087c17fad
miguel Lopes <mzramnadev@gmail.com>
2022-01-18 19:17:30 -0300

    codigo de execução de benchmark corrigido e atualizado

--- scripts/executar_benchmark_tcc.py
+++ scripts/executar_benchmark_tcc.py
@@ -6,12 +6,12 @@ from datetime import datetime
 
 logstash_data={"host":"192.168.0.116","port":5000}
 infos_docker={
-    "maquina_1":{
-        "url":"192.168.0.100",
+    "maquina_arm":{
+        "url":"192.168.0.10",
         "port_docker_sock":2375,
-        "mariadb_id":"",
+        "mariadb_id":"alpine-mariadb-1",
         "mariadb_connect":{
-            "ip":"192.168.0.100",
+            "ip":"192.168.0.10",
             "user":"mzramna",
             "password":"safePassword",
             "database":"sakila",
@@ -19,9 +19,9 @@ infos_docker={
             "tipo":0,
             "sql_file":"containers_build/mysql default exemple.sql"
         },
-        "postgres_id":"",
+        "postgres_id":"alpine-postgres-1",
         "postgres_connect":{
-            "ip":"192.168.0.100",
+            "ip":"192.168.0.10",
             "user":"mzramna",
             "password":"safePassword",
             "database":"sakila",
@@ -30,12 +30,12 @@ infos_docker={
             "sql_file":"containers_build/postgres default exemple.sql"
         },
         },
-    "maquina_2":{
-        "url":"192.168.0.100",
+    "maquina_amd":{
+        "url":"192.168.0.20",
         "port_docker_sock":2375,
-        "mariadb_id":"",
+        "mariadb_id":"alpine-mariadb-1",
         "mariadb_connect":{
-            "ip":"192.168.0.100",
+            "ip":"192.168.0.10",
             "user":"mzramna",
             "password":"safePassword",
             "database":"sakila",
@@ -43,9 +43,9 @@ infos_docker={
             "tipo":0,
             "sql_file":"containers_build/mysql default exemple.sql"
         },
-        "postgres_id":"",
+        "postgres_id":"alpine-postgres-1",
         "postgres_connect":{
-            "ip":"192.168.0.100",
+            "ip":"192.168.0.20",
             "user":"mzramna",
             "password":"safePassword",
             "database":"sakila",
@@ -73,35 +73,35 @@ def executar_teste(ip,user,password,database,port,tipo,sql_file,total_elementos=
 def stop_container(ip:str="",port:int=0,id:str="",compiled:dict={},id_key=""):
     if compiled != {}:
         client = docker.DockerClient(base_url=compiled["url"]+":"+str(compiled["port_docker_sock"]),version="auto")
-        client.stop(compiled[id_key])
+        client.containers.get(compiled[id_key]).start()
     else:
-        if ip is "" or port is 0 or id is "":
+        if ip == "" or port == 0 or id_key == "":
             return None
         client = docker.DockerClient(base_url=ip+":"+str(port),version="auto")
-        client.stop(id)
+        client.containers.get(compiled[id]).stop()
 
 def start_container(ip:str="",port:int=0,id:str="",compiled:dict={},id_key=""):
     if compiled != {}:
         client = docker.DockerClient(base_url=compiled["url"]+":"+str(compiled["port_docker_sock"]),version="auto")
-        client.start(compiled[id_key])
+        client.containers.get(compiled[id_key]).start()
     else:
-        if ip is "" or port is 0 or id is "":
+        if ip == "" or port == 0 or id_key == "":
             return None
         client = docker.DockerClient(base_url=ip+":"+str(port),version="auto")
-        client.start(id)
+        client.containers.get(compiled[id]).start()
 
 def start_test(tipo_bd:str,paralel=False):
-    start_container(compiled=infos_docker["maquina_1"],id_key=tipo_bd+"_id")
-    start_container(compiled=infos_docker["maquina_2"],id_key=tipo_bd+"_id")
-    if paralel is True:
-        dados={infos_docker["maquina_1"][tipo_bd+"_connect"],infos_docker["maquina_2"][tipo_bd+"_connect"]}
+    start_container(compiled=infos_docker["maquina_arm"],id_key=tipo_bd+"_id")
+    start_container(compiled=infos_docker["maquina_amd"],id_key=tipo_bd+"_id")
+    if paralel == True:
+        dados={infos_docker["maquina_arm"][tipo_bd+"_connect"],infos_docker["maquina_amd"][tipo_bd+"_connect"]}
         p=Paralel(total_threads=2)
         p.execute(elementos=dados,function=executar_teste)
     else:
-        executar_teste(**infos_docker["maquina_1"][tipo_bd+"_connect"])
-        executar_teste(**infos_docker["maquina_2"][tipo_bd+"_connect"])
-    stop_container(compiled=infos_docker["maquina_1"],id_key=tipo_bd+"_id")
-    stop_container(compiled=infos_docker["maquina_2"],id_key=tipo_bd+"_id")
+        executar_teste(**infos_docker["maquina_arm"][tipo_bd+"_connect"])
+        executar_teste(**infos_docker["maquina_amd"][tipo_bd+"_connect"])
+    stop_container(compiled=infos_docker["maquina_arm"],id_key=tipo_bd+"_id")
+    stop_container(compiled=infos_docker["maquina_amd"],id_key=tipo_bd+"_id")
 
 #executar testes no bd mariadb
 start_test("mariadb")
