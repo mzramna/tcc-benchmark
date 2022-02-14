3b0a5bb4494b37e4fd8108c173b3283b88fc2060
miguel Lopes <mzramnadev@gmail.com>
2022-02-14 07:49:32 -0300

    teste de tempo corrigido

--- testes/teste_tempo.py
+++ testes/teste_tempo.py
@@ -17,14 +17,11 @@ except:
 total_por_ciclo=4
 maximo_elementos=3000
 incremento=100
-if "quantidade_elementos" not in loaded.keys():
-    loaded["quantidade_elementos"]=0
 if "tempos" not in loaded.keys():
     loaded["tempos"]=[]
-while loaded["quantidade_elementos"]<maximo_elementos:
+while (len(loaded["tempos"]))*incremento<maximo_elementos:
     if  len(loaded["tempos"])<1 or len(loaded["tempos"][-1])>=total_por_ciclo :
         loaded["tempos"].append([])
-    loaded["quantidade_elementos"]+=incremento
     while len(loaded["tempos"][-1])<total_por_ciclo:
         timer=Timer()
         f = open(arquivo+".log", "a")
@@ -35,7 +32,7 @@ while loaded["quantidade_elementos"]<maximo_elementos:
         timer.inicio()
         gerador=GeradorDeSql(sqlite_db="scripts/teste_tempo_db.db",sql_file_pattern="scripts/sqlitePattern.sql", log_file="scripts/teste_tempo.log",level=40,logging_pattern='%(asctime)s - %(name)s - %(levelname)s - %(message)s',logstash_data=logstash_data)
 
-        gerador.gerar_todos_dados_por_json(select_country="pt_br",quantidade_final=loaded["quantidade_elementos"])
+        gerador.gerar_todos_dados_por_json(select_country="pt_br",quantidade_final=(len(loaded["tempos"]))*incremento)
 
         duracao=timer.fim()
         print(duracao)
@@ -48,19 +45,23 @@ while loaded["quantidade_elementos"]<maximo_elementos:
         del gerador
     #pprint(tempos)
     f = open(arquivo+".log", "a")
-    print("para ",loaded["quantidade_elementos"]," dados foram gastos os seguintes dados de tempo")
+    print("para ",len(loaded["tempos"])*incremento," dados foram gastos os seguintes dados de tempo")
     print("media geração completa ",str(sum(loaded["tempos"][-1])/len(loaded["tempos"][-1])))
-    print("media por elemento ",str((sum(loaded["tempos"][-1])/len(loaded["tempos"][-1]))/loaded["quantidade_elementos"]))
-    f.write("para "+str(loaded["quantidade_elementos"])+" dados foram gastos os seguintes dados de tempo"+"\n")
+    print("media por elemento ",str((sum(loaded["tempos"][-1])/len(loaded["tempos"][-1]))/(len(loaded["tempos"]))*incremento))
+    f.write("para "+str((len(loaded["tempos"]))*incremento)+" dados foram gastos os seguintes dados de tempo"+"\n")
     f.write("media geração completa "+str(sum(loaded["tempos"][-1])/len(loaded["tempos"][-1]))+"\n")
-    f.write("media por elemento "+str((sum(loaded["tempos"][-1])/len(loaded["tempos"][-1]))/loaded["quantidade_elementos"])+"\n")
+    f.write("media por elemento "+str((sum(loaded["tempos"][-1])/len(loaded["tempos"][-1]))/(len(loaded["tempos"]))*incremento)+"\n")
+    f.close()
 quantidade_elementos=0
 somatorio_total=0
 quantidades=0
+i=0
+#(len(loaded["tempos"]))*incremento<maximo_elementos
 while quantidade_elementos<maximo_elementos:
     somatorio_total+=sum(loaded["tempos"][i])
     quantidades+=len(loaded["tempos"][i])
     quantidade_elementos+=incremento
+    i+=1
 f = open(arquivo+".log", "a")
 print("media geração completa "+str(somatorio_total/quantidades)+"\n")
 print("media por elemento "+str((somatorio_total/quantidades)/quantidade_elementos)+"\n")
